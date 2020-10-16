545a1a9ae7974a383d058a513d0bf24dbaebc1ed
DevVatu <devvatu@gmail.com>
2020-10-16 11:36:09 +0200

    Updates

---
+++ precache-manifest.950fc51fbc09aca1537e13f5a05bc8d8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "51a451bb5a803de19e886ca67d503e8c",
+    "url": "/raven-hotel/index.html"
+  },
+  {
+    "revision": "cb07774e70c82fa69b7b",
+    "url": "/raven-hotel/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "11a372a509bb1db47da1",
+    "url": "/raven-hotel/static/js/2.b8b33e5e.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/raven-hotel/static/js/2.b8b33e5e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cb07774e70c82fa69b7b",
+    "url": "/raven-hotel/static/js/main.347285d2.chunk.js"
+  },
+  {
+    "revision": "d790a958e7325685fbc3",
+    "url": "/raven-hotel/static/js/runtime-main.d2abdef8.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/raven-hotel/static/media/logo.5d5d9eef.svg"
+  }
+]);
